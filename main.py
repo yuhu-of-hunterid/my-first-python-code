0411fe8f8692ba7b0d351c6ffe4ba6da0205c292
yuhu-of-hunterid <muzamilnasor455@gmail.com>
2025-10-03 20:41:02 +0700

    my first python code

--- main.py
+++ main.py
@@ -7,17 +7,24 @@
 
 # sekuensial
 print('ibu berkata, "pergi ke toko"')
-print('Budi menjawab, "Baik, apa yang harus saya lakukan dii toko?"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu menjawab, "Beli satu botol susu"')
 print("maka Budi berangkat ke toko")
 print("Dan mulai berbelanja")
 
 # percabangan
-jumlah_botol_susu = 173
+jumlah_botol_susu = 10
+jumlah_telur = 10
+print(f"jumlah botol susu {jumlah_botol_susu} botol")
+print(f"jumlah telur {jumlah_telur} butir")
+
 
 if jumlah_botol_susu >0:
     print("Budi mengecek harganya, dan ternyata uangbya cukup")
-    print("Budi membeli 1 botol susu")
+    if jumlah_telur ==0:
+        print("Budi membeli 1 botol  susu saja ")
+    else:
+        print("Budi membeli 1 botol susu dan 6 butir telur")
 else:
     print("Budi tidak jadi membeli 1 botol susu")
 
