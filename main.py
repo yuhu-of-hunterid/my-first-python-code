048d42567c04ca5d55669cf23ef5f78e7b8d810d
yuhu-of-hunterid <muzamilnasor455@gmail.com>
2025-10-03 19:59:12 +0700

    my first python code

--- main.py
+++ main.py
@@ -1,6 +1,28 @@
-"""
-ini adalah demoo project pertama dengan python
-"""
 
-print("hello world!")
-print("my name is Eko")
+
+
+
+
+
+
+# sekuensial
+print('ibu berkata, "pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan dii toko?"')
+print('Ibu menjawab, "Beli satu botol susu"')
+print("maka Budi berangkat ke toko")
+print("Dan mulai berbelanja")
+
+# percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu >0:
+    print("Budi mengecek harganya, dan ternyata uangbya cukup")
+    print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang kerumah")
+print("Menyampaikan hasilnya pada ibu")
+
+
+
