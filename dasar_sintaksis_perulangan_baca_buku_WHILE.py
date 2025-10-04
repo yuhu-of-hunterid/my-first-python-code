17b781b2fcfe34ecd1b24abdb7cfc819e3b0967e
yuhu-of-hunterid <muzamilnasor455@gmail.com>
2025-10-04 17:25:20 +0700

    sekuensial dan percabangan while nggk jls

--- dasar_sintaksis_perulangan_baca_buku_WHILE.py
+++ dasar_sintaksis_perulangan_baca_buku_WHILE.py
@@ -1,16 +1,24 @@
 '''
 program perulangan membaca buku dengan while
 '''
-
 jumlah_buku = 10
 print('ibu berkata, "baca semua bukumu"')
+total_jumlah_baca = 0
 
-jumlah_buku_yang_sudah_dibaca = 0
-print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+jumlah_paham = 0
+print(f'jumlah buku yang sudah dibaca dan di pahami {jumlah_paham}')
 
-while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
-    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
-    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+while total_jumlah_baca < jumlah_buku * 2:
+    total_jumlah_baca = total_jumlah_baca + 1
+    if jumlah_paham == 9:
+        print(f"buku ke {jumlah_paham} belum paham")
+    else:
+        jumlah_paham = jumlah_paham + 1
+        print(f"Buku ke {jumlah_paham} sudah dibaca dan dipahami")
 
-
-print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
+print(f'jumlah buku yang sudah dibaca {jumlah_paham}')
+if jumlah_paham == jumlah_buku:
+    print('"Bu,semua buku sudah dibaca dan dipahami')
+else:
+    print(f'"Bu, tidak semua buku bisa dipahami. '
+          f'Budi hanya bisa memahami {jumlah_paham} buku')
